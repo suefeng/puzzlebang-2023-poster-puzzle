566913f06d07f9aeccdc744a33250e50f5e5fab3
Sue Feng <suefengtan@gmail.com>
2023-08-16 08:43:02 -0500

    Poster puzzle first commit

---
+++ poster.js
@@ -0,0 +1,9 @@
+const grid = document.getElementById('grid');
+const puzzle_string = 'PCAESASREAHOYNCKODREPDPTUHIRSLPEALSSOWAOGOLDRTDNELIGHTTIKMEESJWIZLQGVRUMOTREASURE';
+const puzzle_array = puzzle_string.split('');
+
+puzzle_array.forEach((letter) => {
+  const span = document.createElement('span');
+  grid.appendChild(span);
+  span.innerHTML = letter;
+})
